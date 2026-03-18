605e58554e51770c7fb576a7942f5897f73aef38
ForsIng <forsing@gmail.com>
2026-03-18 20:48:03 +0100

    Update k_29_Struc2Vec.py

--- k_29_Struc2Vec.py
+++ k_29_Struc2Vec.py
@@ -26,7 +26,7 @@ from qiskit_machine_learning.utils import algorithm_globals
 from qiskit.circuit.library import ZZFeatureMap, TwoLocal
 from qiskit.quantum_info import Statevector, Pauli
 
-CSV_PATH = "/Users/4c/Desktop/GHQ/data/loto7hh_4580_k21.csv"
+CSV_PATH = "/data/loto7hh_4580_k21.csv"
 
 df = pd.read_csv(CSV_PATH)
 print()
@@ -252,7 +252,7 @@ def main():
     print()
     """
     Predikcija (Struc2Vec + kvantna regresija, deterministički, strukturno):
-    (1, 2, 7, 8, 23, 26, 34)
+    (1, 2, x, y, z, 26, 34)
 
     Score: 0.4879825338249706
     """
